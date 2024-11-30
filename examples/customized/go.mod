f2ff3e646f32bf7e8341386a2f12187a1a8cfcf1
Malte Poll <1780588+malt3@users.noreply.github.com>
2024-11-30 15:36:45 +0100

    example -> examples

--- examples/customized/go.mod
+++ examples/customized/go.mod
@@ -1,4 +1,4 @@
-module github.com/tweag/credential-helper/example/customized
+module github.com/tweag/credential-helper/examples/customized
 
 go 1.23.3
 
--- examples/customized/helper/helperfactory/helperfactory.go
+++ examples/customized/helper/helperfactory/helperfactory.go
@@ -2,7 +2,7 @@ package helperfactory
 
 import (
 	"github.com/tweag/credential-helper/api"
-	"github.com/tweag/credential-helper/example/customized/helper/authenticate"
+	"github.com/tweag/credential-helper/examples/customized/helper/authenticate"
 )
 
 func CustomHelperFactory(_ string) (api.Helper, error) {
