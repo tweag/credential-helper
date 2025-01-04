a2ba1f7abe59da1f0a1b469aa5f18a82e8276c51
Malte Poll <1780588+malt3@users.noreply.github.com>
2025-01-04 12:03:55 +0100

    detach agent process on Windows

--- agent/client_windows.go
+++ agent/client_windows.go
@@ -7,11 +7,16 @@ import (
 	"syscall"
 )
 
+const (
+	DETACHED_PROCESS         = 0x00000008
+	CREATE_NEW_PROCESS_GROUP = 0x00000200
+)
+
 func procAttrForAgentProcess(stdout, stderr *os.File) *os.ProcAttr {
 	return &os.ProcAttr{
 		Files: []*os.File{nil, stdout, stderr},
 		Sys: &syscall.SysProcAttr{
-			CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
+			CreationFlags: DETACHED_PROCESS | CREATE_NEW_PROCESS_GROUP,
 		},
 	}
 }
