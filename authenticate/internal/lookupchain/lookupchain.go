c8e32b0997b4c12c8eab86f0344c3e774984ebee
Malte Poll <1780588+malt3@users.noreply.github.com>
2025-02-22 19:47:56 +0100

    add setup instructions for remoteapis

--- authenticate/internal/lookupchain/lookupchain.go
+++ authenticate/internal/lookupchain/lookupchain.go
@@ -52,6 +52,25 @@ func (c *LookupChain) Lookup(binding string) (string, error) {
 	return "", fmt.Errorf("no value found for binding %q after querying %v", binding, strings.Join(sourceNames, ", "))
 }
 
+func (c *LookupChain) SetupInstructions(binding, meaning string) string {
+	if len(c.config) == 0 {
+		return fmt.Sprintf("No sources are configured to look up the secret with binding name %s. Refer to the documentation related to configuration files for more information.", binding)
+	}
+	instructions := []string{fmt.Sprintf("Instructions for setting up the secret with binding name %q (%s):", binding, meaning)}
+	for i, entry := range c.config {
+		source, err := c.sourceFor(entry)
+		if err != nil {
+			instructions = append(instructions, fmt.Sprintf("failed to lookup instuctions for entry %d: %v", i, err))
+			continue
+		}
+		instruction, ok := source.SetupInstructions(binding)
+		if ok {
+			instructions = append(instructions, instruction)
+		}
+	}
+	return strings.Join(instructions, "\n")
+}
+
 func (c *LookupChain) sourceFor(entry ConfigEntry) (Source, error) {
 	decoder := json.NewDecoder(bytes.NewReader(entry.RawMessage))
 	decoder.DisallowUnknownFields()
@@ -91,6 +110,7 @@ type ConfigEntry struct {
 type Source interface {
 	Lookup(binding string) (string, error)
 	Canonicalize()
+	SetupInstructions(binding string) (string, bool)
 }
 
 type Env struct {
@@ -120,6 +140,21 @@ func (e *Env) Canonicalize() {
 	if e.Binding == "" {
 		e.Binding = "default"
 	}
+}
+
+func (e *Env) SetupInstructions(binding string) (string, bool) {
+	if e.Binding != binding {
+		return "", false
+	}
+	_, success := os.LookupEnv(e.Name)
+	var status string
+	if success {
+		status = "SET"
+	} else {
+		status = "NOT SET"
+	}
+
+	return fmt.Sprintf(" - Export the environment variable %s (status: %s)", e.Name, status), true
 }
 
 type Keyring struct {
@@ -152,6 +187,24 @@ func (k *Keyring) Canonicalize() {
 	if k.Binding == "" {
 		k.Binding = "default"
 	}
+}
+
+func (e *Keyring) SetupInstructions(binding string) (string, bool) {
+	if e.Binding != binding {
+		return "", false
+	}
+	_, getErr := keyring.Get(e.Service, "")
+	var status string
+	if errors.Is(getErr, keyring.ErrNotFound) {
+		status = "NOT SET"
+	} else if getErr != nil {
+		status = "ERROR ACCESSING KEYCHAIN"
+	} else {
+		status = "SET"
+	}
+
+	return fmt.Sprintf(` - Add the secret to the system keyring under the %s service name (status: %s):
+    $ %s setup-keyring -f secret.txt %s`, e.Service, status, os.Args[0], e.Service), true
 }
 
 // Default constructs a partially marshalled Config from a slice of specific config entries.
